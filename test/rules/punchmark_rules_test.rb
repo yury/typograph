89444447686ff30cce48eba5c8318c82c56eeb45
Yury Korolev <yury.korolev@gmail.com>
2009-08-15 04:03:55 +0400

    added punchmark rules

---
+++ test/rules/punchmark_rules_test.rb
@@ -0,0 +1,40 @@
+#kcode utf-8
+require "test/unit"
+require File.dirname(__FILE__) + '/../../lib/rules/punchmark_rules'
+
+class PunchmarkRulesTest < Test::Unit::TestCase
+
+  def test_auto_comma
+    assert_equal "вы будете, а я нет", PunchmarkRules.run("вы будете а я нет")
+    assert_equal "вы будете, а я нет", PunchmarkRules.run("вы будете, а я нет")
+  end
+
+  def test_punctuation_marks_limit
+    assert_equal "hello!!!", PunchmarkRules.run("hello!!!!!!!!!!!!!!!!!!")
+    assert_equal "WTF???", PunchmarkRules.run("WTF???????")
+  end
+
+  def test_punctuation_marks_base_limit
+    assert_equal ":", PunchmarkRules.run(":::")
+    assert_equal ",", PunchmarkRules.run(",,")
+  end
+
+  def test_hellip
+    assert_equal "&hellip;", PunchmarkRules.run("..")
+    assert_equal "&hellip;", PunchmarkRules.run("...")
+    assert_equal "&hellip;", PunchmarkRules.run("....")
+  end
+
+  def test_eng_apostrophe
+    assert_equal "she&rsquo;s", PunchmarkRules.run("she's")
+  end
+
+  def test_fix_brackets
+    assert_equal "(круто)", PunchmarkRules.run("( круто )")
+  end
+
+  def test_replacements
+    
+  end
+
+end
